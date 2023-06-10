b4b24e30cf3811c451b00a87a6679996e521c41b
Matthias Buttgereit <matthias.buttgereit@outlook.com>
2023-06-11 00:34:31 +0200

    Draw filled triangles

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,31 @@
 pub mod wavefront;
 
-use image::RgbImage;
+use image::{Rgb, RgbImage};
 use wavefront::WavefrontObject;
 
-pub fn draw_triangle(image: &mut RgbImage, triangle: &Triangle, color: [u8; 3]) {
-    let (x0, y0) = triangle.0;
-    let (x1, y1) = triangle.1;
-    let (x2, y2) = triangle.2;
+fn draw_triangle(image: &mut RgbImage, triangle: &Triangle, color: [u8; 3]) {
+    let ((x0, y0), (x1, y1)) = get_bounding_box(triangle);
 
-    draw_line(image, (x0, y0), (x1, y1), color);
-    draw_line(image, (x1, y1), (x2, y2), color);
-    draw_line(image, (x2, y2), (x0, y0), color);
-}
-
-pub fn draw_line(image: &mut RgbImage, start: (u32, u32), end: (u32, u32), color: [u8; 3]) {
-    let (mut x0, mut y0) = start;
-    let (mut x1, mut y1) = end;
-
-    let mut steep = false;
-
-    if x0.abs_diff(x1) < y0.abs_diff(y1) {
-        (x0, y0) = (y0, x0);
-        (x1, y1) = (y1, x1);
-        steep = true;
-    }
-
-    if x0 > x1 {
-        (x0, x1) = (x1, x0);
-        (y0, y1) = (y1, y0);
-    }
-
-    let (dx, dy) = (x0.abs_diff(x1), y0.abs_diff(y1));
-    let derror2 = dy * 2;
-    let mut error2: i32 = 0;
-
-    let mut y = y0;
-
-    for x in x0..x1 {
-        if steep {
-            image.put_pixel(y, x, image::Rgb(color));
-        } else {
-            image.put_pixel(x, y, image::Rgb(color));
-        }
-
-        error2 += derror2 as i32;
-        if error2 > dx as i32 {
-            if y1 > y0 {
-                y += 1
-            } else {
-                y -= 1
-            };
-            error2 -= dx as i32 * 2;
+    for x in x0..=x1 {
+        for y in y0..=y1 {
+            if triangle.contains((x, y)) {
+                image.put_pixel(x, y, Rgb(color));
+            }
         }
     }
 }
 
-pub fn map_xy_to_image((x, y): (f32, f32), img: &RgbImage) -> (u32, u32) {
+fn get_bounding_box(triangle: &Triangle) -> ((u32, u32), (u32, u32)) {
+    let x_min = triangle.a.0.min(triangle.b.0.min(triangle.c.0));
+    let x_max = triangle.a.0.max(triangle.b.0.max(triangle.c.0));
+
+    let y_min = triangle.a.1.min(triangle.b.1.min(triangle.c.1));
+    let y_max = triangle.a.1.max(triangle.b.1.max(triangle.c.1));
+
+    ((x_min, y_min), (x_max, y_max))
+}
+
+fn map_xy_to_image((x, y): (f32, f32), img: &RgbImage) -> (u32, u32) {
     let (x, y) = ((x + 1.0) / 2.0, (y + 1.0) / 2.0);
     let dim = img.dimensions();
     let (x, y) = (x * dim.0 as f32, y * dim.1 as f32);
@@ -86,10 +56,32 @@ pub fn draw_object(object: WavefrontObject, img: &mut RgbImage) {
 }
 
 #[derive(Default, Debug)]
-pub struct Triangle((u32, u32), (u32, u32), (u32, u32));
+pub struct Triangle {
+    a: (u32, u32),
+    b: (u32, u32),
+    c: (u32, u32),
+}
 
 impl Triangle {
     pub fn new(a: (u32, u32), b: (u32, u32), c: (u32, u32)) -> Self {
-        Self(a, b, c)
+        Self { a, b, c }
+    }
+
+    pub fn contains(&self, (x, y): (u32, u32)) -> bool {
+        let p1 = (self.a.0 as f32, self.a.1 as f32);
+        let p2 = (self.b.0 as f32, self.b.1 as f32);
+        let p3 = (self.c.0 as f32, self.c.1 as f32);
+        let x = x as f32;
+        let y = y as f32;
+
+        let area_triangle =
+            0.5 * (-p2.1 * p3.0 + p1.1 * (p3.0 - p2.0) + p1.0 * (p2.1 - p3.1) + p2.0 * p3.1);
+
+        let s = 1.0 / (2.0 * area_triangle)
+            * (p1.1 * p3.0 - p1.0 * p3.1 + (p3.1 - p1.1) * x + (p1.0 - p3.0) * y);
+        let t = 1.0 / (2.0 * area_triangle)
+            * (p1.0 * p2.1 - p1.1 * p2.0 + (p1.1 - p2.1) * x + (p2.0 - p1.0) * y);
+
+        s >= 0.0 && t >= 0.0 && s + t <= 1.0
     }
 }
