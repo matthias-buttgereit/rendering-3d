5387cb02bfcc22b5c8256b751217001432d5e9c5
Matthias Buttgereit <matthias.buttgereit@outlook.com>
2023-08-06 12:19:09 +0200

    Updated pictures

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let width: u32 = 1920;
     let height: u32 = 1920;
 
-    let name = "diablo3_pose";
+    let name = "african_head";
 
     let model = format!("{name}.obj");
     let texture = format!("{name}_diffuse.tga");
@@ -12,5 +12,7 @@ fn main() {
     let mut object = Object::new(&model);
     object.set_texture(&texture);
 
-    object.render_to_image("test.png", width, height).unwrap();
+    object
+        .render_to_image("african_head.png", width, height)
+        .unwrap();
 }
